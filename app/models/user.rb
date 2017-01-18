c6ba1ae2453b79f7f74c8da9c7a09d0aa9d06964
Radar PGCS <neloangelo@bol.com.br>
2017-01-18 18:20:41 -0200

    Cria entidade usuário.

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User
+  include Mongoid::Document
+
+  field :registry, type: Integer
+  field :password, type: String
+  field :status, type: String
+  field :status_note, type: String
+  field :last_login, type: Time
+  field :version_visited, type: String
+
+  validates :registry, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5, maximum: 20 }
+  validates :status, presence: true, inclusion: { in: %w(ACTIVE INACTIVE BLOCKED) }
+
+  def ==(obj)
+    return false if obj.nil?
+    return false unless self.class == obj.class
+    
+    (self.registry == obj.registry)
+  end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,107 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test 'should not be equal' do
+    user = User.new registry: 12345
+
+    u = nil
+    assert_not user == u
+
+    u = User.new
+    assert_not user == u
+  end
+
+  test 'should be equal' do
+    user = User.new registry: 12345
+
+    assert user == user
+    u = user.dup
+    assert user == u
+
+    u = User.new registry: 12345
+    assert user == u
+  end
+
+  test 'should validate registry presence' do
+    user = User.new do |u|
+      u.password = '12345'
+      u.status = 'ACTIVE'
+    end
+
+    assert user.invalid?
+
+    errors = user.errors.messages
+    assert_equal 1, errors[:registry].size
+    assert_equal 'não pode ficar em branco', errors[:registry].first
+  end
+
+  test 'should validate password presence' do
+    user = User.new do |u|
+      u.registry = '12345'
+      u.status = 'ACTIVE'
+    end
+
+    assert user.invalid?
+
+    errors = user.errors.messages
+    assert_equal 2, errors[:password].size
+    assert_equal 'não pode ficar em branco', errors[:password].first
+  end
+
+  test 'should validate password minimum size' do
+    minimum = 5
+    user = User.new do |u|
+      u.registry = '12345'
+      u.password = ('0' * (minimum - 1))
+      u.status = 'ACTIVE'
+    end
+
+    assert user.invalid?
+
+    errors = user.errors.messages
+    assert_equal 1, errors[:password].size
+    assert_equal "é muito curto (mínimo: #{minimum} caracteres)", errors[:password].first
+  end
+
+  test 'should validate password maximum size' do
+    maximum = 20
+    user = User.new do |u|
+      u.registry = '12345'
+      u.password = ('0' * (maximum + 1))
+      u.status = 'ACTIVE'
+    end
+
+    assert user.invalid?
+
+    errors = user.errors.messages
+    assert_equal 1, errors[:password].size
+    assert_equal "é muito longo (máximo: #{maximum} caracteres)", errors[:password].first
+  end
+
+  test 'should validate status presence' do
+    user = User.new do |u|
+      u.registry = '12345'
+      u.password = '12345'
+    end
+
+    assert user.invalid?
+
+    errors = user.errors.messages
+    assert_equal 2, errors[:status].size
+    assert_equal 'não pode ficar em branco', errors[:status].first
+  end
+
+  test 'should validate status value inclusion' do
+    user = User.new do |u|
+      u.registry = '12345'
+      u.password = '12345'
+      u.status = 'WRONG'
+    end
+
+    assert user.invalid?
+
+    errors = user.errors.messages
+    assert_equal 1, errors[:status].size
+    assert_equal 'não está incluído na lista', errors[:status].first
+  end
+end
