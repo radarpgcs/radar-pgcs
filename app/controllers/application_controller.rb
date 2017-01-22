9740e57ab7baa4e17b981e655a006d1004bce490
Radar PGCS <neloangelo@bol.com.br>
2017-01-22 18:16:14 -0200

    Autenticação.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
+require Rails.root.join 'app', 'services', 'security_service.rb'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include LoginConcern
 
   # GET /
   def index
@@ -9,5 +13,28 @@ class ApplicationController < ActionController::Base
   # GET /login
   def login
     render '/login', layout: false
+  end
+
+  # POST /authenticate
+  def sign_in
+    user = authenticate_login params[:identity], params[:password]
+    if user
+      log_in user
+      redirect_to caller_url
+      flash.clear
+
+      Rails.logger.info "User #{user.registry} has just signed in."
+    else
+      flash[:danger] = t 'login.authentication_failed.message'
+      render '/login', layout: false
+    end
+  end
+
+  # DELETE /logout
+  def sign_out
+    #message = (logged_in?) ? "User #{current_user.cpf} has just signed out." : 'Session destroyed by expiration.'
+    #log_out
+    #info message
+    #redirect_to home_path
   end
 end
---
+++ app/controllers/concerns/login_concern.rb
@@ -0,0 +1,38 @@
+module LoginConcern
+  extend ActiveSupport::Concern
+
+  def authenticate_login(uid, pass)
+    user = User.where(registry: uid).first
+    if user.nil?
+      Rails.logger.info "Login: Unknown user identification #{uid}"
+      return
+    end
+
+    auth_ok = _password_match? user, pass
+    Rails.logger.info "Login: Invalid password to user with identification #{uid}" unless auth_ok
+
+    user
+  end
+
+  def log_in(user)
+    session[:user_so] = {
+      :registry => user.registry
+    }
+  end
+
+  def caller_url
+    regex = /\Ahttps?:\/\/(#{request.host}):?(#{request.port})?/
+    if (request.referer =~ regex) == 0
+      caller_path = request.referer.sub regex, ''
+      ((caller_path != login_path) && (caller_path != signin_path)) ? caller_path : home_path
+    else
+      home_path
+    end
+  end
+
+  private
+  def _password_match?(user, raw_password)
+    pass_hash = Services::Security.generate_hash(raw_password, user.salt_number)
+    user.password == pass_hash
+  end
+end
