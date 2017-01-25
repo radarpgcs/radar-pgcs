af6fb75c6301dcd112381ba961df8d2fe5474643
Radar PGCS <neloangelo@bol.com.br>
2017-01-25 17:08:39 -0200

    Login.

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -13,4 +13,19 @@ class ApplicationControllerTest < ActionController::TestCase
     assert_response :success
     assert_template 'login', layout: nil
   end
+
+  test 'should not sign in' do
+    post :sign_in, params: { identity: '00000000000', password: '123' }
+    assert_template 'login', layout: nil
+    assert_equal I18n.translate('login.authentication_failed.message'), flash[:danger]
+  end
+
+  test 'should sign in' do
+    user = User.all.first
+    post :sign_in, params: { identity: user.registry, password: user.password }
+    
+    assert_not flash[:danger]
+    assert_response 302
+    assert_redirected_to home_path
+  end
 end
