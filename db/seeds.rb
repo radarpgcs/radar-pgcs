4891ef45bb1739c2c4c066dc4011d980d638b947
Radar PGCS <neloangelo@bol.com.br>
2017-01-29 11:57:13 -0200

    Carrega changelog do projeto no banco de dados.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+init = Time.now
 raise 'Could not find environment variable ADMIN_ID' if ENV['ADMIN_ID'].nil?
 
 User.new do |u|
@@ -6,3 +7,18 @@ User.new do |u|
   u.status = 'INACTIVE'
   u.roles = %w(ADMINISTRATOR COLLABORATOR MEMBER)
 end.save! unless User.where(registry: ENV['ADMIN_ID']).exists?
+
+changelog_dir = Rails.root.join 'metadata', 'changelog'
+News.delete_all
+puts 'Rewriting news collection'
+
+Dir.entries(changelog_dir).each do |changelog|
+  next if changelog.match(/\A\.{1,2}\z/)
+  File.readlines(File.join changelog_dir, changelog).each do |line|
+    News.new(version: changelog, message: line.strip).save!
+  end
+end
+
+puts
+puts 'Seeding finished!'
+puts Time.now - init
