6e983f7549cc1892222365bd62540b82d23568c8
Radar PGCS <neloangelo@bol.com.br>
2017-02-27 23:32:25 -0300

    Prepara layout do rodapé.

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -7,10 +7,10 @@ module MenuHelper
     return if request[:menu_header_built]
     
     menu = []
-    menu << _break_tab('<nav class="navbar navbar-toggleable-md fixed-top">')
+    menu << '<nav class="navbar navbar-toggleable-md fixed-top">'
     menu.concat(_button_icon)
     menu << _home_menu_item(options)
-    menu.concat(_container_menu_items(options))
+    menu.concat(_container_menu_items_header(options))
     menu << _break_tab('</nav>')
     menu.concat(_container_title)
     
@@ -18,8 +18,16 @@ module MenuHelper
     menu.join.html_safe
   end
 
-  def build_menu_footer
+  def build_menu_footer(options = {})
+    return if request[:menu_footer_built]
 
+    menu = []
+    menu << '<nav class="navbar navbar-toggleable-md">'
+    menu.concat(_container_menu_items_footer(options))
+    menu << _break_tab('</nav>')
+
+    request[:menu_footer_built] = true
+    menu.join.html_safe
   end
 
   private
@@ -30,14 +38,14 @@ module MenuHelper
     text << text_excerpt
   end
 
-  def _container_menu_items(options)
+  def _container_menu_items_header(options)
     container = []
     container << _break_tab('<div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">', 1)
     container << _break_tab('<ul class="navbar-nav">', 2)
 
     options[:menu_items] ||= []
     menu_items = options[:menu_items].collect do |item|
-      _menu_item item[:label], item[:path]
+      _menu_item_header item[:label], item[:path]
     end
     container.concat menu_items
     container.concat _authentication_menu_item
@@ -46,12 +54,37 @@ module MenuHelper
     container << _break_tab('</div>', 1)
   end
 
-  def _menu_item(label, path)
+  def _container_menu_items_footer(options)
+    container = []
+    container << _break_tab('<div class="collapse navbar-collapse" id="navbarCollapse">', 1)
+    container << _break_tab('<ul class="navbar-nav">', 2)
+
+    options[:menu_items] ||= []
+    menu_items = options[:menu_items].collect do |item|
+      _menu_item_footer item[:label], item[:path]
+    end
+    container.concat menu_items
+    container.concat _show_modal_news unless session[:user_so].nil?
+
+    container << _break_tab('</ul>', 2)
+    container << _break_tab('</div>', 1)
+  end
+
+  def _menu_item_header(label, path)
     menu = []
     menu << _break_tab('<li class="nav-item">', 3)
     menu << _break_tab('<h5>', 4)
     menu << link_to(label, path, class: 'nav-link')
     menu << '</h5>'
+    menu << _break_tab('</li>', 3)
+  end
+
+  def _menu_item_footer(label, path)
+    menu = []
+    menu << _break_tab('<li class="nav-item">', 3)
+    menu << _break_tab('<h6>', 4)
+    menu << link_to(label, path, class: 'nav-link')
+    menu << '</h6>'
     menu << _break_tab('</li>', 3)
   end
 
@@ -83,6 +116,21 @@ module MenuHelper
     menu << _break_tab('<h5>', 4)
     menu << link_to(t('menu.header.sign_out'), signout_path, method: :delete, class: 'nav-link')
     menu << '</h5>'
+    menu << _break_tab('</li>', 3)
+  end
+
+  def _show_modal_news
+    menu = []
+    menu << _break_tab('<li class="nav-item">', 3)
+    menu << _break_tab('<h6>', 4)
+
+    link = '<a href="javascript: void(0)" data-toggle="modal" data-target="#modalVersionMessage" '
+    link << 'onclick="showModalNews()" class="nav-link">'
+    link << ENV['APP_VERSION']
+    link << '</a>'
+
+    menu << link
+    menu << '</h6>'
     menu << _break_tab('</li>', 3)
   end
 
