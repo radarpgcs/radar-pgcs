c285ebb1b38887d0b9681acb829c733eb1a0a677
Radar PGCS <neloangelo@bol.com.br>
2017-07-11 16:06:11 -0300

    Cria teste de unidade para a classe promotions_helper.

---
+++ test/helpers/promotions_helper_test.rb
@@ -0,0 +1,17 @@
+class PromotionsHelperTest < ActionView::TestCase
+  include PromotionsHelper
+
+  test 'should get a list of options to select component with all years' do
+    first = Rails.configuration.radarpgcs[:first_promotion_year]
+    last = Rails.configuration.radarpgcs[:last_promotion_year]
+    options = years_for_selection.split("\n")
+
+    assert options.first.match(%r(<option value="">Por favor selecione</option>))
+    options.delete_at 0
+
+    years = (first..last).map { |year| year }
+    for i in 0..(years.size - 1)
+      assert options[i].match(/<option value="#{years[i]}">#{years[i]}<\/option>/)
+    end
+  end
+end
