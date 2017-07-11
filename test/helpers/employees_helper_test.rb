42ee55c5e91ba912b5ebda6eeee496ab2607cca5
Radar PGCS <neloangelo@bol.com.br>
2017-07-11 10:52:18 -0300

    Corrige integração mal-sucedida.

--- test/helpers/employees_helper_test.rb
+++ test/helpers/employees_helper_test.rb
@@ -41,16 +41,10 @@ class EmployeesHelperTest < ActionView::TestCase
   test "should get employee's promotion dashboard" do
     employee = Employee.find_by registry: 2
     proms = { pm: 0, pts: 0 }
-<<<<<<< HEAD
     last_promotion_year = Rails.configuration.radarpgcs[:last_promotion_year]
 
     employee.promotions.each do |p|
       next if p.year < employee.hiring_date.year || p.year > last_promotion_year
-=======
-
-    employee.promotions.each do |p|
-      next if p.year < employee.hiring_date.year
->>>>>>> b23f363d8b3a2d3ad6ef7bc29011c8c349a9123a
 
       if p.type == 'PM'
         proms[:pm] += 1
